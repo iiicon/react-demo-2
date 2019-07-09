d76254ffbeae72eeb984b3f06f5be4cc05da2da9
shiguangwei <shigw5420@joyowo.com>
2019-07-09 23:40:33 +0800

    class demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+class Box extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      number: 1
+    }
+  }
+  add() {
+    this.setState({
+      number: this.state.number + 1
+    })
+  }
+  render() {
+    return (
+      <div className="red">
+        <span>{this.state.number}</span>
+        <button onClick={this.add.bind(this)}>+</button>
+        <strong>{this.props.name}</strong>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box name="box is god"/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
